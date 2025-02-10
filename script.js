72be4f7d93bab0455a774fc0fe28e033c25fcf45
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-02-10 07:42:53 -0500

    Finish one column of 16 squares

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container');
+
+function addDiv() {
+    const newDiv = document.createElement('div');
+
+    // append the newly created element to the container
+    container.appendChild(newDiv);
+}
+
+// possibly will need a loop that will do this same thing 16 times for one row
+    for (let i = 0; i < 16; i++) {
+        addDiv();
+    }
+
+// another loop that will simply replicate the one row 16 times? 
