3c90d9d18222e913967d5b51477dabf87d1926f2
inkwill8 <sebastien.lacasse101@gmail.com>
2025-02-20 19:29:00 -0500

    Fix grid resize
    The grid now resizes when you input a new number because I am passing
    that input into the singleRow() and multipleRow() functions, but the
    new boxes are being constrained by the width of the container.
    
    Also, the event listeners do not get added to the newly created boxes.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// some vars
 const container = document.querySelector('.container');
+let gridSize = 16;
 
 function addDiv() {
     const newDiv = document.createElement('div');
@@ -8,19 +10,19 @@ function addDiv() {
 }
 
 // possibly will need a loop that will do this same thing 16 times for one row
-   function singleRow() { 
-    for (let i = 0; i < 16; i++) {
+   function singleRow(size) { 
+    for (let i = 0; i < size; i++) {
         addDiv();
    }
 }
 // another loop that will simply replicate the one row 16 times?
-    function multipleRows() {
-        for (let i = 0; i < 16; i++) {
-            singleRow();
+    function multipleRows(size) {
+        for (let i = 0; i < size; i++) {
+            singleRow(size);
         }
     }
 
-multipleRows();
+multipleRows(gridSize);
 
 // hover effects
 
@@ -38,17 +40,18 @@ for (let box of boxes) {
 
 const btn = document.getElementById('btn');
 
-let gridSize = 16;
-
 function getUserInput() {
     gridSize = prompt('Please input the new grid size.');
     
     if (gridSize > 100) {
         do {
-            gridSize = prompt('Please define a size less than 100 by 100.');
+            gridSize = prompt('Please define a size that is 100 or less.');
         } while (gridSize > 100); 
         
-    }}
+    } else {
+        multipleRows(gridSize);
+    }
+}
 
 btn.addEventListener('click', getUserInput);
 
